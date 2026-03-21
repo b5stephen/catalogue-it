e2b3c9000de3710d4386f6b30014281d029936b2
Stephen Denekamp <stephen@etched.dev>
2026-03-22 11:23:39 +1300

    Refix iphone compact view back issue

--- catalogue-it/Items/ItemListView.swift
+++ catalogue-it/Items/ItemListView.swift
@@ -15,7 +15,31 @@ struct ItemListView: View {
     let showWishlistBadge: Bool
     @Binding var selectedItem: CatalogueItem?
 
+#if !os(macOS)
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+#endif
+
     var body: some View {
+#if !os(macOS)
+        if horizontalSizeClass == .compact {
+            List {
+                ForEach(items) { item in
+                    ItemRowView(item: item, catalogue: catalogue, showWishlistBadge: showWishlistBadge)
+                        .tag(item)
+                        .contentShape(Rectangle())
+                        .onTapGesture { selectedItem = item }
+                }
+            }
+            .listStyle(.plain)
+        } else {
+            regularList
+        }
+#else
+        regularList
+#endif
+    }
+
+    private var regularList: some View {
         List(selection: $selectedItem) {
             ForEach(items) { item in
                 ItemRowView(item: item, catalogue: catalogue, showWishlistBadge: showWishlistBadge)
